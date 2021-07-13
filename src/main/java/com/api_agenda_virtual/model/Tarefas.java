c67eea840b85d4a51d31ab30b77c710df1786e1b
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-07-13 18:58:03 -0300

    Adicionando models

---
+++ src/main/java/com/api_agenda_virtual/model/Tarefas.java
@@ -0,0 +1,25 @@
+package com.api_agenda_virtual.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Tarefas {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int idTarefa;
+	
+	private String nome;
+	private String decricao;
+	private Data dataEntrega;
+
+}
